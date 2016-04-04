66a0de323669f7e33165c5d82ff8b150aaafd3c4
Shaun Farrell <Shaun.Farrell@vista.co>
2016-04-04 17:12:17 +1200

    Removed extra imports

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import './app.routes';
 
 // Directives
 import './shared/menu';
-import './shared/sidebar';
 
 // Components
-//import './components/feeds/routes';
-//import './components/feed/routes';
-import './components/feeds';
+import './components/dashboard';
